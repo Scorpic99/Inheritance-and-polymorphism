ddb4efc2848aee813fd2582c6cf2baddec757522
Sasha Bagley <volkadav99@list.ru>
2022-12-08 23:37:16 +0300

    update task2

--- Task 2. Shapes. Sides and corners/Task 2. Shapes. Sides and corners.cpp
+++ Task 2. Shapes. Sides and corners/Task 2. Shapes. Sides and corners.cpp
@@ -1,30 +1,13 @@
 ﻿#include <iostream>
 
-void print_shapes(Triangle &vlt, Quadrilateral &qua, std::string nameShapes) {
-    if (nameShapes == "Треугольник") {
-        int a, b, c, A, B, C;
-        vlt.get_triangle(a, b, c, A, B, C);
-        std::cout << vlt.get_name() << ":" << std::endl;
-        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
-    }
-    else if (nameShapes == "Четырехугольник") {
-        int a, b, c, d, A, B, C, D;
-        qua.get_quadrilateral(a, b, c, d, A, B, C, D);
-        std::cout << qua.get_name() << ":" << std::endl;
-        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
-    }
-    
 
-}
 
 class Shapes {
 public:
-    std::string get_name() {
+    virtual std::string get_name() {
         return name;
     }
-    int get_sides() {
+    virtual int get_sides() {
         return sides;
     }
     Shapes() {
@@ -38,8 +21,11 @@ protected:
 
 class Triangle : public Shapes {
 public:
-    std::string get_name() {
+    std::string get_name() override{
         return name;
+    }
+    virtual int get_sides() override {
+        return sides;
     }
     void get_triangle(int& a, int& b, int& c, int& A, int& B, int& C) {
         a = this->a;
@@ -48,9 +34,6 @@ public:
         A = this->A;
         B = this->B;
         C = this->C;
-    }
-    int get_sides() {
-        return sides;
     }
     Triangle() {
         sides = 3;
@@ -106,10 +89,13 @@ public:
 
 class Quadrilateral : public Shapes {
 public:
-    std::string get_name() {
+    std::string get_name() override {
         return name;
     }
-    void get_quadrilateral(int& a, int& b, int& c, int& d, int& A, int& B, int& C, int& D) {
+    int get_sides() override {
+        return sides;
+    }
+    void get_quadrilateral(int& a, int& b, int& c, int& d, int& A, int& B, int& C, int& D){
         a = this->a;
         b = this->b;
         c = this->c;
@@ -118,9 +104,6 @@ public:
         B = this->B;
         C = this->C;
         D = this->D;
-    }
-    int get_sides() {
-        return sides;
     }
     Quadrilateral() {
         sides = 4;
@@ -179,7 +162,6 @@ public:
 class Rhomb : public Quadrilateral {//ромб
 public:
     Rhomb(int a, int b, int c, int d, int A, int B) {
-        
         this->a = a;
         this->b = b;
         this->c = c;
@@ -192,58 +174,81 @@ public:
     }
 };
 
+void print_shapes(Shapes* shapes, std::string nameShapes) {
+    if (nameShapes == "Треугольник") {
+        int a = 0, b = 0, c = 0, A = 0, B = 0, C = 0;
+        Triangle().get_triangle(a, b, c, A, B, C);
+        std::cout << shapes->get_name() << ":" << std::endl;
+        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
+        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+    }
+    else if (nameShapes == "Четырехугольник") {
+        int a = 0, b = 0, c = 0, d = 0, A = 0, B = 0, C = 0, D = 0;
+        //shapes = new Quadrilateral;
+        //Quadrilateral quad;
+        Quadrilateral().get_quadrilateral(a, b, c, d, A, B, C, D);
+        std::cout << shapes->get_name() << ":" << std::endl;
+        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
+        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    }
+
+
+}
+
 int main()
 {
     setlocale(LC_ALL, "Rus");
     int a = 50, b = 60, c = 70, d = 55, A = 45, B = 80, C = 75, D = 66;
-
-    Shapes shapes;
-    //Triangle triangle;
-    //Quadrilateral quadrilateral;
-
+    int a2 = 0, b2 = 60, c2 = 0, d2 = 0, A2 = 0, B2 = 0, C2 = 0, D2 = 0;
+    
+    Shapes* shapes = new Shapes;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
 
     RectangularTriangle RT(a, b, c, A, B);
-    //print_shapes(triangle, quadrilateral ,"Треугольник");
-    RT.get_triangle(a, b, c, A, B, C);
-	std::cout << RT.get_name() << ":" << std::endl;
-	std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-	std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+    RT.get_triangle(a2, b2, c2, A2, B2, C2);
+	//std::cout << RT.get_name() << ":" << std::endl;
+	std::cout << "Стороны: a=" << a2 << " b=" << b2 << " c=" << c2 << std::endl;
+	std::cout << "Углы: A=" << A2 << " B=" << B2 << " C=" << C2 << std::endl;
+    print_shapes(shapes, "Треугольник");
+
     IsoscelesTriangle IT(a, b, A, B, C);
-    //print_shapes(triangle, quadrilateral ,"Треугольник");
-    IT.get_triangle(a, b, c, A, B, C);
-    std::cout << IT.get_name() << ":" << std::endl;
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+    print_shapes(shapes, "Треугольник");
+    //IT.get_triangle(a, b, c, A, B, C);
+    //std::cout << IT.get_name() << ":" << std::endl;
+    //std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
+    //std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
     EquilateralTriangle ET(a);
-    //print_shapes(triangle, quadrilateral ,"Треугольник");
-    ET.get_triangle(a, b, c, A, B, C);
-    std::cout << ET.get_name() << ":" << std::endl;
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+    print_shapes(shapes, "Треугольник");
+    //ET.get_triangle(a, b, c, A, B, C);
+    //std::cout << ET.get_name() << ":" << std::endl;
+    //std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
+    //std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
     Rectangle Rec(a, b);
-    //print_shapes(triangle, quadrilateral ,"Четырехугольник");
-    Rec.get_quadrilateral(a, b, c, d, A, B, C, D);
+    print_shapes(shapes, "Четырехугольник");
+    /*Rec.get_quadrilateral(a, b, c, d, A, B, C, D);
     std::cout << Rec.get_name() << ":" << std::endl;
     std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;*/
     Square S(a);
-    //print_shapes(triangle, quadrilateral ,"Четырехугольник");
-    S.get_quadrilateral(a, b, c, d, A, B, C, D);
+    print_shapes(shapes, "Четырехугольник");
+    /*S.get_quadrilateral(a, b, c, d, A, B, C, D);
     std::cout << S.get_name() << ":" << std::endl;
     std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
     std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;*/
     Parallelogram P(a, b, A, B);
-    //print_shapes(triangle, quadrilateral ,"Четырехугольник");
-    P.get_quadrilateral(a, b, c, d, A, B, C, D);
+    print_shapes(shapes,"Четырехугольник");
+    /*P.get_quadrilateral(a, b, c, d, A, B, C, D);
     std::cout << P.get_name() << ":" << std::endl;
     std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;*/
     Rhomb R(a, b, c, d, A, B);
-    //print_shapes(triangle, quadrilateral ,"Четырехугольник");
-    R.get_quadrilateral(a, b, c, d, A, B, C, D);
+    print_shapes(shapes,"Четырехугольник");
+    /*R.get_quadrilateral(a, b, c, d, A, B, C, D);
     std::cout << R.get_name() << ":" << std::endl;
     std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
     std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    */
 
 }
